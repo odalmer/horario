64c9c3e83482e78519c7fb5e0f9ae2e0974b947f
Odalmer Pimentel <48610363+odalmer@users.noreply.github.com>
2022-09-02 21:17:59 -0700

    sidebar added

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-// const materias = {
-//     Algebra:'link'
+const menuBtn = document.getElementById("menuBtn");
+const menu = document.getElementById("menu");
+const container = document.getElementById("container");
+const horario = document.getElementById("horario");
 
-// }
+let menuStatus;
+
+menuBtn.addEventListener("click", () => {
+  if (menu.style.display == "block") {
+    menuStatus = "none";
+    container.style.gridTemplateColumns = "auto";
+  } else {
+    menuStatus = "block";
+    container.style.gridTemplateColumns = "20rem 67.5%";
+  }
+  menu.style.display = menuStatus;
+});
